220b082aeb23899eb0bc48024be9517b34dd93a7
michaelboyas <michael.boyas@kellogg.northwestern.edu>
2022-01-18 17:29:33 +0000

    work from class

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,46 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts
+DROP TABLE IF EXISTS salesperson
+DROP TABLE IF EXISTS companies
+DROP TABLE IF EXISTS industries
+DROP TABLE IF EXISTS activities
+
 -- CREATE TABLES
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  title TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_id INTEGER,
+  salesperson_id INTEGER
+);
+
+CREATE TABLE salesperson (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  industry_id INTEGER
+);
+
+CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT,
+  date_date DATETIME
+  notes TEXT,
+  contact_id INTEGER,
+  salesperson_id INTEGER,
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,41 @@
+-- Drops existing tables, so we start fresh with each
+-- run of this script
+-- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS posts
+DROP TABLE IF EXISTS likes
+DROP TABLE IF EXISTS comments
+DROP TABLE IF EXISTS followers
+
+-- CREATE TABLES
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+);
+
+CREATE TABLE posts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  image_url TEXT,
+  user_id INTEGER
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER
+  post_id INTEGER
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  comment TEXT
+  user_id INTEGER
+  post_id INTEGER
+);
+
+CREATE TABLE followers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  follower_id INTEGER
+  user_id INTEGER
+);
